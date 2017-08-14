d9a93000f513945d05a8b3687b7042d420f327f2
Peter Thompson <petethegreat@gmail.com>
2017-08-14 14:57:06 -0400

    bah

--- n2.R
+++ n2.R
@@ -9,68 +9,7 @@ library(tm)
 library(tau)
 library(dplyr)
 
-OldGetWordCounts<-function(source=c('blogs','news','twitter'),split=20,testfrags=1,tokenize)
-{
 
-    # source is either blogs, news or twitter
-    # load the source as textdata
-    # split into split parts (cut(seq_along(textdata)),split=split)
-    # create a corpus for each file
-    infile<-sprintf('./data/final/en_US/en_US.%s.txt',source)
-    outfile<-sprintf('./data/n1_wordcount_%s.txt',source)
-    testfile<-sprintf('./data/test_%s.txt',source)
-
-    # load lines
-    textData<-readLines(infile,n=1000)
-    #sample to split into train and test
-
-    mycorpus<-VCorpus(VectorSource(textData))
-        # clean it
-    mycorpus<-tm_map(mycorpus,content_transformer(tolower))
-    mycorpus<-tm_map(mycorpus,removeNumbers)
-    #mycorpus<-tm_map(mycorpus,removeWords, stopwords("english"))
-    mycorpus<-tm_map(mycorpus,removePunctuation)
-    mycorpus<-tm_map(mycorpus,stripWhitespace)
-
-    cleaned<-lapply(mycorpus,as.character)
-
-    totalcounts<-NULL
-
-
-    #do lines one at a time
-    for (lineno in 1:1000)
-    {
-        cat('processing line ',lineno,'\n')
-        counts<-textcnt(cleaned[[lineno]],method='string',n=1)
-
-        if( is.null(totalcounts))
-        {
-            totalcounts<-data.frame(counts=unclass(counts),words=names(unclass(counts)),stringsAsFactors=FALSE)
-        }
-        else
-        {
-            tempcounts<-data.frame(counts=unclass(counts),words=names(unclass(counts)),stringsAsFactors=FALSE)
-            present<- tempcounts$words %in% totalcounts$words
-            totalcounts['words' == tempcounts$words[present],'counts']<- 
-                totalcounts['words' == tempcounts$words[present],'counts'] + tempcounts[present,'counts']
-            if (sum(present) < length(present))
-            {
-                totalcounts<-bind_rows(totalcounts,tempcounts[!present,])
-            }
-
-
-        }
-
-
-    }
-
-    counts<-textcnt(cleaned,method='string',n=1)
-
-    # df<-data.frame(counts=unclass(counts))
-    write.csv(totalcounts,file='./test.csv')
-
-
-}
 
 trainTestValSplit<-function(trainfrac=0.8,testfrac=0.1,seedval=37)
 {
@@ -109,7 +48,118 @@ trainTestValSplit<-function(trainfrac=0.8,testfrac=0.1,seedval=37)
 
 }
 
-GetWordCounts<-function(source=c('blogs','news','twitter'),n=1,lines=1000)
+GetWordCounts<-function(source=c('blogs','news','twitter'),n=1,rmstop=FALSE,temps=20,usetemps=20)
+{
+
+    # source is either blogs, news or twitter
+    # load the source as textdata
+    # split into split parts (cut(seq_along(textdata)),split=split)
+    # create a corpus for each file
+    stopstr<-''
+    if (rmstop)
+    {
+            stopstr<-'_rmstop'
+    }
+    infile<-sprintf('./data/train_%s.txt',source)
+    outfile<-sprintf('./data/n%i_wordcount%s_%s.txt',n,stopstr,source)
+
+    # load lines
+
+    textData<-readLines(infile)
+
+    textData<-iconv(textData, "latin1", "ASCII", sub="BADCHAR")
+    textData<-gsub('[^ ]*(BADCHAR)+[^ ]','',textData)
+
+
+    #sample to split into train and test
+
+    mycorpus<-VCorpus(VectorSource(textData))
+        # clean it
+    mycorpus<-tm_map(mycorpus,content_transformer(tolower))
+    mycorpus<-tm_map(mycorpus,removeNumbers)
+    #mycorpus<-tm_map(mycorpus,removeWords, stopwords("english"))
+    mycorpus<-tm_map(mycorpus,removePunctuation)
+    mycorpus<-tm_map(mycorpus,stripWhitespace)
+
+    cleaned<-lapply(mycorpus,as.character)
+    rm(mycorpus)
+    rm(textData)
+    cat('cleaned corpus\n')
+
+
+    cuts<-cut(runif(length(cleaned)),breaks=temps,labels=FALSE)
+
+    mem<-sapply(mget(ls()),object.size)
+    cat(mem,'\n')
+    for(i in 1:usetemps)
+    {
+        cat('counting ',i,' of ',dotemps,'\n')
+        counts<-textcnt(cleaned[cuts==i],method='string',n=nval)
+        meh<-unclass(counts)
+        words<-names(counts)
+        df<-data.frame(term=words,counts=meh)
+        write.csv(df,file=sprintf('./temp/temp_%i.csv',i),row.names=FALSE)
+    }
+
+    rm(cleaned)
+
+    df<-read.csv(sprintf('./temp/temp_%i.csv',i),stringsAsFactors=FALSE)
+
+    for(i in 2:usetemps)
+    {
+        cat('merging ',i,' of ',usetemps,'\n')
+        df2<-read.csv(sprintf('./temp/temp_%i.csv',i),stringsAsFactors=FALSE)
+        df<-merge(df,df2,by.x='term',by.y='term',all=TRUE,suffixes=c('','.new'))
+        df$counts<-rowSums(df[,c('counts','counts.new')])
+        df<-subset(df,select=c('term','counts'))
+        rm(df2)
+    }
+    cat('merging complete, sorting\n')
+
+    df<-df[order(df$counts,decreasing=TRUE),]
+    write.csv(df,file=outfile,row.names=FALSE)
+    cat('wrote to ',outfile)
+
+}
+
+# 100k works for blogs.
+# 100k - about 45 secs
+# 200k - about 90 secs
+
+
+nval<-1
+nolines<-200000
+GetWordCounts(source='twitter',n=1,temps=40,usetemps=2)
+
+# trainTestValSplit()
+# 150s to do 2-grams at 200k lines
+
+# cat('doing blogs\n')
+# time<-system.time(GetWordCounts('blogs',n=nval,lines=nolines))
+# cat('time: ',time,'\n')
+
+# cat('doing news\n')
+# time<-system.time(GetWordCounts('news',n=nval,lines=nolines))
+# cat('time: ',time,'\n')
+
+# cat('doing twitter\n')
+# time<-system.time(GetWordCounts('twitter',n=nval,lines=nolines))
+# cat('time: ',time,'\n')
+
+# nval<-4
+# cat('doing blogs\n')
+# time<-system.time(GetWordCounts('blogs',n=nval,lines=nolines))
+# cat('time: ',time,'\n')
+
+# cat('doing news\n')
+# time<-system.time(GetWordCounts('news',n=nval,lines=nolines))
+# cat('time: ',time,'\n')
+
+# cat('doing twitter\n')
+# time<-system.time(GetWordCounts('twitter',n=nval,lines=nolines))
+# cat('time: ',time,'\n')
+
+OldGetWordCounts<-function(source=c('blogs','news','twitter'),n=1,lines=1000)
 {
 
     # source is either blogs, news or twitter
@@ -154,40 +204,3 @@ GetWordCounts<-function(source=c('blogs','news','twitter'),n=1,lines=1000)
     cat('wrote to ',outfile)
 
 }
-
-# 100k works for blogs.
-# 100k - about 45 secs
-# 200k - about 90 secs
-
-
-nval<-3
-nolines<-200000
-trainTestValSplit()
-# 150s to do 2-grams at 200k lines
-
-# cat('doing blogs\n')
-# time<-system.time(GetWordCounts('blogs',n=nval,lines=nolines))
-# cat('time: ',time,'\n')
-
-# cat('doing news\n')
-# time<-system.time(GetWordCounts('news',n=nval,lines=nolines))
-# cat('time: ',time,'\n')
-
-# cat('doing twitter\n')
-# time<-system.time(GetWordCounts('twitter',n=nval,lines=nolines))
-# cat('time: ',time,'\n')
-
-# nval<-4
-# cat('doing blogs\n')
-# time<-system.time(GetWordCounts('blogs',n=nval,lines=nolines))
-# cat('time: ',time,'\n')
-
-# cat('doing news\n')
-# time<-system.time(GetWordCounts('news',n=nval,lines=nolines))
-# cat('time: ',time,'\n')
-
-# cat('doing twitter\n')
-# time<-system.time(GetWordCounts('twitter',n=nval,lines=nolines))
-# cat('time: ',time,'\n')
-
-
