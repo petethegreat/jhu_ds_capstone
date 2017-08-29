97d5b9f957663a95f9a9013060f4744802fc29e2
Peter Thompson <petethegreat@gmail.com>
2017-08-29 17:27:15 -0400

    moose

--- model.R
+++ model.R
@@ -352,6 +352,21 @@ dflist<-lapply(1:nmax,loaddf)
 
         }
 
+
+        # if we didn't find a context, then we don't recognize the immediatly preceding words from bigrams (or higher)
+        # could either use the most frequent word, or the most frequent 
+        if (!foundcontext)
+        {
+            # will go based on continuations
+            bigrams<-dflist[[2]]
+            wordprobs<-bigrams[,.(w2,count=.N),by=w2]
+            wordprobs<-wordprobs[,prob:=count/nrow(bigrams)]
+            wordprobs<-wordprobs[order(-prob),.(word=w2,prob)]
+            return(wordprobs[1:10])
+
+        }
+
+
         # cat(head(highcounts),'\n')
         # print(head(highcounts))
         wordprobs<-highcounts[,.(word,prob = (count-Dval)/sum(count))]
@@ -395,13 +410,17 @@ dflist<-lapply(1:nmax,loaddf)
         # print(head(wordprobs[order(-prob)]))
 
         # do lowest order/unigrams
+        # each word receives equal weighting, lamda/V (V = vocab size, or nrows in )
         unicounts<-dflist[[1]]
+        unicounts<-unicounts[word !='UNKNOWN']
         unicounts<-unicounts[,.(word=w1,prob=lval/nrow(unicounts))]
         wordprobs<-merge(wordprobs,unicounts,by.x='word',by.y='word',all=TRUE,suffixes=c('.total','.new'))
         wordprobs<-wordprobs[,.(word,prob=prob.total %+na% prob.new)]
+        wordprobs<-wordprobs[order(-prob)]
 
 
-        print(head(wordprobs[order(-prob)]))
+
+        return(wordprobs[1:10])
 
     }
 
@@ -424,7 +443,9 @@ dflist<-lapply(1:nmax,loaddf)
         words<-strsplit(thestring,' ')
         for (vec in words)
         {
-            predictCleaned(vec)
+            
+            results<-predictCleaned(vec)
+            print(results)
         }
         # this returns a list of character vectors, each vector containing single words
 
@@ -435,15 +456,15 @@ dflist<-lapply(1:nmax,loaddf)
     }
 
     predict(c(
-        "When you breathe, I want to be the air for you. I'll be there for you, I'd live and I'd",
-        "Guy at my table's wife got up to go to the bathroom and I asked about dessert and he started telling me about his",
-        "I'd give anything to see arctic monkeys this",
-        "Talking to your mom has the same effect as a hug and helps reduce your",
-        "When you were in Holland you were like 1 inch away from me but you hadn't time to take a",
-        "I'd just like all of these questions answered, a presentation of evidence, and a jury to settle the",
-        "I can't deal with unsymetrical things. I can't even hold an uneven number of bags of groceries in each",
-        "Every inch of you is perfect from the bottom to the",
-        "I’m thankful my childhood was filled with imagination and bruises from playing",
+        # "When you breathe, I want to be the air for you. I'll be there for you, I'd live and I'd",
+        # "Guy at my table's wife got up to go to the bathroom and I asked about dessert and he started telling me about his",
+        # "I'd give anything to see arctic monkeys this",
+        # "Talking to your mom has the same effect as a hug and helps reduce your",
+        # "When you were in Holland you were like 1 inch away from me but you hadn't time to take a",
+        # "I'd just like all of these questions answered, a presentation of evidence, and a jury to settle the",
+        # "I can't deal with unsymetrical things. I can't even hold an uneven number of bags of groceries in each",
+        # "Every inch of you is perfect from the bottom to the",
+        # "I’m thankful my childhood was filled with imagination and bruises from playing",
         "I like how the same people are in almost all of Adam Sandler's"
         ))
 
